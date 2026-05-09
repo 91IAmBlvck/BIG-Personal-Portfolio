2607d0c3a5832298ea835b2a663f719e464b0e9b
Bandile Gama <gamabandile91@gmail.com>
2026-05-09 19:17:01 +0200

    Initial portfolio upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BIG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
