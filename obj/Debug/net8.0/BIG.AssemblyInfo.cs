2279c560eaf527b76c91d15ab5eeff787e274e0b
Bandile Gama <gamabandile91@gmail.com>
2026-05-09 23:51:02 +0200

    fix education timeline-content

--- obj/Debug/net8.0/BIG.AssemblyInfo.cs
+++ obj/Debug/net8.0/BIG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea18e98ae14c64babdb89e670ab5710f58949706")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c0e9cc6d7f4d7589ff166d5413a02759fb58d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
