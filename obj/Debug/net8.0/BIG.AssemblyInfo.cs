858d4239feb41bada9125d42b9e4b18e4e5b5247
Bandile Gama <gamabandile91@gmail.com>
2026-05-10 06:42:58 +0200

    maintanance

--- obj/Debug/net8.0/BIG.AssemblyInfo.cs
+++ obj/Debug/net8.0/BIG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d773f2463f6f02205cd92186485faaf83ab153b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db02de3902c023a784e90016c40632990e073acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
