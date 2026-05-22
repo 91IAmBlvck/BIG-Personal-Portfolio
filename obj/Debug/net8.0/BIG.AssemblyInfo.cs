78ffabc5dd079cb31914909db3ea90890e037611
Bandile Gama <gamabandile91@gmail.com>
2026-05-23 01:48:10 +0200

    CV d

--- obj/Debug/net8.0/BIG.AssemblyInfo.cs
+++ obj/Debug/net8.0/BIG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee42443a39b8609991d5577bd7a7fab786e0f6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f2ef2eeb8f2010a5e0e6e1278a6394eac9ceb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
