4855fc546d19d92d46a2f5de2af4580c06825de3
Bandile Gama <gamabandile91@gmail.com>
2026-05-23 08:46:58 +0200

    BIG DONE

--- obj/Debug/net8.0/BIG.AssemblyInfo.cs
+++ obj/Debug/net8.0/BIG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ffabc5dd079cb31914909db3ea90890e037611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60106960f19b1e1ad13b490a22e64745135c1317")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
